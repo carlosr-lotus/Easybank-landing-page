db4b055c057a51fb4bce6424af8d198a369984bc
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-04-04 18:45:12 -0300

    Project File

---
+++ src/components/Header/Navigation.js
@@ -0,0 +1,17 @@
+import './navigation.css'
+
+export default function NavMobile() {
+    return (
+        <div className="nav-mobile">
+            {/* <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Blog</li>
+                    <li>Careers</li>
+                </ul>
+            </nav> */}
+        </div>
+    )
+}
